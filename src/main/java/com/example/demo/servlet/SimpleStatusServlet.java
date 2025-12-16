aa17a6821f1cc3ed5caecfde1ba663c81f086bd2
bala-amypo <bala@amypo.in>
2025-12-16 13:41:50 +0000

    User commit

--- src/main/java/com/example/demo/servlet/SimpleStatusServlet.java
+++ src/main/java/com/example/demo/servlet/SimpleStatusServlet.java
@@ -1,10 +1,13 @@
 package com.example.demo.servlet;
 
 
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+
 
 
 @WebServlet("/simple-status")
-public class SimpleStatusServlet
+public class SimpleStatusServlet extends HttpServlet
 {
 
 }
