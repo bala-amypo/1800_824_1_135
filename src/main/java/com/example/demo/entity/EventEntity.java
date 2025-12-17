f99510a5eec08dac7854909cb316c041021b8b66
bala-amypo <bala@amypo.in>
2025-12-17 11:59:30 +0000

    User commit

--- src/main/java/com/example/demo/entity/EventEntity.java
+++ src/main/java/com/example/demo/entity/EventEntity.java
@@ -1,29 +1,29 @@
-package com.example.demo.entity;
+// package com.example.demo.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
+// import jakarta.persistence.Entity;
+// import jakarta.persistence.Id;
+// import jakarta.persistence.GeneratedValue;
+// import jakarta.persistence.GenerationType;
 
 
-@Entity
-public class EventEntity
-{
-    @Id
-    @GeneratedValue(strategy=GenerationType.IDENTITY)
-    private Long id;
-    private String title;
-    private String description;
-    private String location;
-    private String category;
-    private String publisher;
-    private boolean isActive;
-    private Timestamp createdAt;
+// @Entity
+// public class EventEntity
+// {
+//     @Id
+//     @GeneratedValue(strategy=GenerationType.IDENTITY)
+//     private Long id;
+//     private String title;
+//     private String description;
+//     private String location;
+//     private String category;
+//     private String publisher;
+//     private boolean isActive;
+//     private Timestamp createdAt;
 
-    public void setId(Long id)
-    {
+//     public void setId(Long id)
+//     {
         
-    }
+//     }
 
 
-}
+// }
