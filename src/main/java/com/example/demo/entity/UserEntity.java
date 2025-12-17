2b9668ff5d685ca947b0a548519d4139399d60a8
bala-amypo <bala@amypo.in>
2025-12-17 08:15:28 +0000

    User commit

--- src/main/java/com/example/demo/entity/UserEntity.java
+++ src/main/java/com/example/demo/entity/UserEntity.java
@@ -1,21 +1,81 @@
 package com.example.demo.entity;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.PrePersist;
+//import jakarta.persistence.Table;
 
-@Entity 
-public class UserEntity
-{
+import java.sql.Timestamp;
+
+@Entity
+//@Table(name = "users")
+public class UserEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
     private String fullName;
+
+    @Column(unique = true)
     private String email;
+
     private String password;
+
     private String role;
+
     private Timestamp createdAt;
 
-    
-    public Long getId()
-    {
-        return id;
+  
+
+    public UserEntity(String fullName, String email, String password, String role) {
+        this.fullName = fullName;
+        this.email = email;
+        this.password = password;
+        this.role = role;
     }
 
+    @PrePersist
+    public void onCreate() {
+        this.createdAt = new Timestamp(System.currentTimeMillis());
+    }
+
+    public Long getId() { return id; }
+    public String getFullName() { return fullName; }
+    public String getEmail() { return email; }
+    public String getPassword() { return password; }
+    public String getRole() { return role; }
+    public Timestamp getCreatedAt() { return createdAt; }
+
+    public void setId(Long id) { 
+        this.id = id; }
+    public void setFullName(String fullName) { 
+        this.fullName = fullName; }
+    public void setEmail(String email) { 
+        this.email = email; }
+    public void setPassword(String password) { 
+        this.password = password; }
+    public void setRole(String role) { 
+        this.role = role; }
+
+     
+    public UserEntity(Long id,
+                String fullName,
+                String email,
+                String password,
+                String role,
+                Timestamp createdAt) {
+
+        this.id = id;
+        this.fullName = fullName;
+        this.email = email;
+        this.password = password;
+        this.role = role;
+        this.createdAt = createdAt;
+    }
+     public User() {
+    }
 }
