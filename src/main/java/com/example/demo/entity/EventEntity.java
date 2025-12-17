649541a6da33a195810cb633aa8dbbfa5522c124
bala-amypo <bala@amypo.in>
2025-12-17 10:13:35 +0000

    User commit

--- src/main/java/com/example/demo/entity/EventEntity.java
+++ src/main/java/com/example/demo/entity/EventEntity.java
@@ -1,18 +1,18 @@
-package com.example.demo.entity;
+// package com.example.demo.entity;
 
-import jakarta.persistence.Entity;
+// import jakarta.persistence.Entity;
 
-@Entity
-public class EventEntity
-{
-    private Long id;
-    private String title;
-    private String description;
-    private String location;
-    private String category;
-    private String publisher;
-    private boolean isActive;
-    private Timestamp createdAt;
+// @Entity
+// public class EventEntity
+// {
+//     private Long id;
+//     private String title;
+//     private String description;
+//     private String location;
+//     private String category;
+//     private String publisher;
+//     private boolean isActive;
+//     private Timestamp createdAt;
 
-    
-}
+
+// }
