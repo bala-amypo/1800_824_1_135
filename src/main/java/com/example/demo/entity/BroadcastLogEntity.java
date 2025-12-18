6da463cf6264cb241a0354d6bbd3c4a0fc9d2d24
bala-amypo <bala@amypo.in>
2025-12-18 06:16:53 +0000

    User commit

--- src/main/java/com/example/demo/entity/BroadcastLogEntity.java
+++ src/main/java/com/example/demo/entity/BroadcastLogEntity.java
@@ -1,8 +1,66 @@
 package com.example.demo.entity;
 
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.PrePersist;
 
-public class BroadcastLogEntity
+
+import java.sql.Timestamp;
+
+
+@Entity
+public class SubscriptionEntity
 {
+    @Id
+    @GeneratedValue(strategy=GenerationType.IDENTITY)
+    private Long id;
+    private String user;
+    private String event;
+    private Timestamp subscribedAt;
+
+    @PrePersist
+    public void onCreate() {
+        this.subscribedAt = new Timestamp(System.currentTimeMillis());
+    }
+
+    public Long getId() {
+         return id; 
+         }
+    public String getUser() {
+         return user; 
+         }
+    public String getEvent() {
+         return event;
+          }
     
+    
+    public Timestamp getSubscribedAt() {
+         return subscribedAt;
+          }
+    
+
+    public void setId(Long id) { 
+        this.id = id;
+         }
+    public void setUser(String user) { 
+        this.user = user;
+         }
+    public void setEvent(String event) { 
+        this.event = event;
+         }
+   
+     
+     
+    public SubscriptionEntity(Long id, String fullName, String email, String password,String role,Timestamp createdAt,Boolean isActive) {
+
+        this.id = id;
+        this.user = user;
+        this.event = event;
+        this.subscribedAt = subscribedAt;
+    }
+     public SubscriptionEntity() {
+    }
 }
