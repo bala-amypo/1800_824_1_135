46d214f8e45287e5dd1e1a0f92ca7daa2a9f3583
bala-amypo <bala@amypo.in>
2025-12-18 04:18:51 +0000

    User commit

--- src/main/java/com/example/demo/entity/SubscriptionEntity.java
+++ src/main/java/com/example/demo/entity/SubscriptionEntity.java
@@ -2,3 +2,98 @@ package com.example.demo.entity;
 
 
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.PrePersist;
+
+
+import java.sql.Timestamp;
+
+
+@Entity
+public class EventUpdateEntity
+{
+    @Id
+    @GeneratedValue(strategy=GenerationType.IDENTITY)
+    private Long id;
+    private String title;
+    private String description;
+    private String location;
+    private String category;
+    private String publisher;
+    private Boolean isActive=true;
+    private Timestamp createdAt;
+
+    @PrePersist
+    public void onCreate() {
+        this.createdAt = new Timestamp(System.currentTimeMillis());
+    }
+
+    public Long getId() {
+         return id; 
+         }
+    public String getTitle() {
+         return title; 
+         }
+    public String getDescription() {
+         return description;
+          }
+    public String getLocation() {
+         return location;
+          }
+    public String getCategory() {
+         return category;
+          }
+    public String getPublisher() {
+         return publisher;
+          }
+    public Boolean getIsActive()
+    { 
+    return isActive;
+    }
+    
+    public Timestamp getCreatedAt() {
+         return createdAt;
+          }
+    
+
+    public void setId(Long id) { 
+        this.id = id;
+         }
+    public void setTitle(String title) { 
+        this.title = title;
+         }
+    public void setDescription(String description) { 
+        this.description = description;
+         }
+    public void setLocation(String location) { 
+        this.location = location; 
+        }
+    public void setCategory(String category) { 
+        this.category = category; 
+        }
+    public void setPublisher(String publisher) { 
+        this.publisher = publisher; 
+        }
+    public void setIsActive(Boolean isActive )
+    { 
+        this.isActive=isActive;
+    }
+
+     
+    public EventUpdateEntity(Long id, String fullName, String email, String password,String role,Timestamp createdAt,Boolean isActive) {
+
+        this.id = id;
+        this.title = title;
+        this.description = description;
+        this.location = location;
+        this.category = category;
+        this.publisher = publisher; 
+        this.isActive=isActive;
+        this.createdAt = createdAt;
+    }
+     public EventUpdateEntity() {
+    }
+}
