73663a20e5b28750e9acfa178e0c1746e361e4e8
bala-amypo <bala@amypo.in>
2025-12-18 17:04:23 +0000

    User commit

--- src/main/java/com/example/demo/controller/UserController.java
+++ src/main/java/com/example/demo/controller/UserController.java
@@ -1,8 +1,40 @@
 package com.example.demo.controller;
 
 
+import com.example.demo.entity.UserEntity;
+import com.example.demo.service.UserService;
 
-public class UserController
-{
-     
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/users")
+public class UserController {
+
+    private final UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @PostMapping("/register")
+    public UserEntity register(@RequestBody UserEntity user) {
+        return userService.registerUser(user);
+    }
+
+    @GetMapping("/{id}")
+    public UserEntity getUser(@PathVariable Long id) {
+        return userService.getUserById(id);
+    }
+
+    @GetMapping
+    public List<UserEntity> getAllUsers() {
+        return userService.getAllUsers();
+    }
 }
