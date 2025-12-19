14d53d4965f10d930f3298908de96d1ca0454ec2
bala-amypo <bala@amypo.in>
2025-12-19 08:59:54 +0000

    User commit

--- src/main/java/com/example/demo/repository/EventRepository.java
+++ src/main/java/com/example/demo/repository/EventRepository.java
@@ -1,5 +1,5 @@
   package com.example.demo.repository;
-import com.example.demo.entity.EventEntity;
+import com.example.demo.entity.Event;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 
