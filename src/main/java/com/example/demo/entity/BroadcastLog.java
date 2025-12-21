b4bbcecbc52e5b2887fbdbe9e145c04d3149f939
bala-amypo <bala@amypo.in>
2025-12-21 17:08:55 +0000

    User commit

--- src/main/java/com/example/demo/entity/BroadcastLog.java
+++ src/main/java/com/example/demo/entity/BroadcastLog.java
@@ -6,7 +6,7 @@ import jakarta.persistence.Id;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.PrePersist;
-
+import jakarta.persistence.ManyToOne;
 
 import java.sql.Timestamp;
 
