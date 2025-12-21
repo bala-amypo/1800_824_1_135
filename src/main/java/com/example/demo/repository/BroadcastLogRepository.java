60174def5c93e0bc184a0b12c0b1e0256ef233ef
bala-amypo <bala@amypo.in>
2025-12-21 16:34:35 +0000

    User commit

--- src/main/java/com/example/demo/repository/BroadcastLogRepository.java
+++ src/main/java/com/example/demo/repository/BroadcastLogRepository.java
@@ -1,10 +1,10 @@
 package com.example.demo.repository;
 import com.example.demo.entity.BroadcastLog;
 import org.springframework.data.jpa.repository.JpaRepository;
-
+import java.util.List;
 
 public interface BroadcastLogRepository extends JpaRepository<BroadcastLog,Long>{
 
-
+      List<BroadcastLog> findEventUpdatedId(Long eventUpdateId);
 
 }
