c8e2e2f688a4d5e3426d9110f50aa2f9092d0bfe
bala-amypo <bala@amypo.in>
2025-12-21 14:37:20 +0000

    User commit

--- src/main/java/com/example/demo/repository/EventRepository.java
+++ src/main/java/com/example/demo/repository/EventRepository.java
@@ -1,11 +1,12 @@
 package com.example.demo.repository;
 import com.example.demo.entity.Event;
 import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.List;
 
 
 public interface EventRepository extends JpaRepository<Event,Long>{
 
-    
+    List<Event> findyIsActiveTrue();
 
 
 }
