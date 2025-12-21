7d7bdcc44d3cc79bbc56d8420962f6123890b6cd
bala-amypo <bala@amypo.in>
2025-12-21 14:57:55 +0000

    User commit

--- src/main/java/com/example/demo/controller/EventController.java
+++ src/main/java/com/example/demo/controller/EventController.java
@@ -2,7 +2,14 @@ package com.example.demo.controller;
 
 import com.example.demo.entity.Event;
 import com.example.demo.service.EventService;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 
 import java.util.List;
 
