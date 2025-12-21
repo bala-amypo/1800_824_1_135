d4941475ebf3ad09fa3626d9a22b796f1e19eb5b
bala-amypo <bala@amypo.in>
2025-12-21 14:40:19 +0000

    User commit

--- src/main/java/com/example/demo/service/impl/EventServiceImpl.java
+++ src/main/java/com/example/demo/service/impl/EventServiceImpl.java
@@ -1,9 +1,52 @@
-package com.example.demo.service.impl;
+ package com.example.demo.service;
 
+import com.example.demo.entity.Event;
+import com.example.demo.repository.EventRepository;
+import org.springframework.stereotype.Service;
 
+import java.util.List;
 
-public class EventServiceImpl
-{
-    
-    
+@Service
+public class EventServiceImpl implements EventService {
+
+    private final EventRepository eventRepository;
+
+    public EventServiceImpl(EventRepository eventRepository) {
+        this.eventRepository = eventRepository;
+    }
+
+    @Override
+    public Event createEvent(Event event) {
+        return eventRepository.save(event);
+    }
+
+    @Override
+    public Event updateEvent(Long id, Event event) {
+        Event existing = getEventById(id);
+
+        existing.setTitle(event.getTitle());
+        existing.setDescription(event.getDescription());
+        existing.setLocation(event.getLocation());
+        existing.setCategory(event.getCategory());
+
+        return eventRepository.save(existing);
+    }
+
+    @Override
+    public Event getEventById(Long id) {
+        return eventRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("Event not found"));
+    }
+
+    @Override
+    public List<Event> getActiveEvents() {
+        return eventRepository.findByIsActiveTrue();
+    }
+
+    @Override
+    public void deactivateEvent(Long id) {
+        Event event = getEventById(id);
+        event.setIsActive(false);
+        eventRepository.save(event);
+    }
 }
