cbdf5a5b476f844a77e8be3e1af25c88d203430b
bala-amypo <bala@amypo.in>
2025-12-24 16:58:36 +0000

    User commit

--- src/main/java/com/example/demo/service/UserService.java
+++ src/main/java/com/example/demo/service/UserService.java
@@ -1,3 +1,17 @@
+// package com.example.demo.service;
+
+// import com.example.demo.entity.User;
+// import java.util.List;
+
+
+// public interface UserService
+// {
+//     User registerUser(User user);
+//     User getUserById(Long id);
+//     List<User> getAllUsers();
+//     User findByEmail(String email);
+    
+// }
  package com.example.demo.service;
 
 import com.example.demo.entity.User;
--- src/main/java/com/example/demo/service/impl/UserServiceImpl.java
+++ src/main/java/com/example/demo/service/impl/UserServiceImpl.java
@@ -1,3 +1,54 @@
+// package com.example.demo.service.impl;
+
+// import com.example.demo.entity.User;
+// import com.example.demo.repository.UserRepository;
+// import com.example.demo.service.UserService;
+// import org.springframework.stereotype.Service;
+// import java.util.List;
+
+
+// @Service 
+// public class UserServiceImpl implements UserService
+// {
+//     private final UserRepository userRepo;
+//     public UserServiceImpl(UserRepository userRepo) 
+//     {
+//         this.userRepo=userRepo;
+//     }
+//     //method1
+//     @Override
+//     public User registerUser(User user)
+//     {
+//     if(userRepo.existsByEmail(user.getEmail()))
+//     {
+//         throw new IllegalArgumentException("Email already exists");
+//     }
+//         return userRepo.save(user);
+//     }
+//     //method2
+
+//     @Override
+//     public User getUserById(Long id)
+//     {
+//         return userRepo.findById(id).orElseThrow(()->new RuntimeException("User not found"));
+//     }
+//     //method3
+    
+//     @Override
+//     public List<User> getAllUsers()
+//     {
+//         return userRepo.findAll();
+//     } 
+//     //method4 
+
+//     @Override
+//     public User findByEmail(String email)
+//     {
+//         return userRepo.findByEmail(email).orElseThrow(()->new RuntimeException("User not found"));
+//     }
+
+
+// }
 package com.example.demo.service.impl;
 
 import com.example.demo.entity.User;
