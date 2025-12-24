585eefd8209734278434ec630bafdcfa03084531
bala-amypo <bala@amypo.in>
2025-12-24 17:10:35 +0000

    User commit

--- src/main/java/com/example/demo/service/EventService.java
+++ src/main/java/com/example/demo/service/EventService.java
@@ -1,18 +1,33 @@
+// package com.example.demo.service;
+
+// import com.example.demo.entity.Event;
+
+// import java.util.List;
+
+// public interface EventService {
+
+//     Event createEvent(Event event);
+
+//     Event updateEvent(Long id, Event event);
+
+//     Event getEventById(Long id);
+
+//     List<Event> getActiveEvents();
+
+//     void deactivateEvent(Long id);
+// }
 package com.example.demo.service;
 
 import com.example.demo.entity.Event;
-
 import java.util.List;
 
 public interface EventService {
 
-    Event createEvent(Event event);
+    Event save(Event event);
 
-    Event updateEvent(Long id, Event event);
+    Event getById(Long id);
 
-    Event getEventById(Long id);
+    List<Event> getAllEvents();
 
-    List<Event> getActiveEvents();
-
-    void deactivateEvent(Long id);
+    Event deactivateEvent(Long id);
 }
