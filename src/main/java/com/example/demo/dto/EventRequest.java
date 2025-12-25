c47be02696066e0b3bf35caeb778d85f67bf3ba6
bala-amypo <bala@amypo.in>
2025-12-25 09:01:19 +0000

    User commit

--- src/main/java/com/example/demo/dto/EventRequest.java
+++ src/main/java/com/example/demo/dto/EventRequest.java
@@ -1,8 +1,37 @@
+// package com.example.demo.dto;
+
+
+
+// public class EventRequest
+// {
+    
+// }
+
 package com.example.demo.dto;
 
+import com.example.demo.entity.SeverityLevel;
 
+public class EventUpdateRequest {
 
-public class EventRequest
-{
-    
+    private String message;
+    private SeverityLevel severityLevel;
+
+    public EventUpdateRequest() {
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public SeverityLevel getSeverityLevel() {
+        return severityLevel;
+    }
+
+    public void setSeverityLevel(SeverityLevel severityLevel) {
+        this.severityLevel = severityLevel;
+    }
 }
--- src/main/java/com/example/demo/dto/EventUpdateRequest.java
+++ src/main/java/com/example/demo/dto/EventUpdateRequest.java
@@ -1,8 +1,38 @@
+// package com.example.demo.dto;
+
+
+
+// public class EventUpdateRequest
+// {
+    
+// }
+
+
 package com.example.demo.dto;
 
+import com.example.demo.entity.SeverityLevel;
 
+public class EventUpdateRequest {
 
-public class EventUpdateRequest
-{
-    
+    private String message;
+    private SeverityLevel severityLevel;
+
+    public EventUpdateRequest() {
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public SeverityLevel getSeverityLevel() {
+        return severityLevel;
+    }
+
+    public void setSeverityLevel(SeverityLevel severityLevel) {
+        this.severityLevel = severityLevel;
+    }
 }
