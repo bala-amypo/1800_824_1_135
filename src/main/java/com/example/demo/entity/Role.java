181bd93d757fba4934c2041669eeac9993f0932f
bala-amypo <bala@amypo.in>
2025-12-25 05:58:18 +0000

    User commit

--- src/main/java/com/example/demo/entity/Role.java
+++ src/main/java/com/example/demo/entity/Role.java
@@ -1,13 +1,14 @@
+// // package com.example.demo.entity;
+
+// // public enum Role {
+// //     ADMIN,
+// //     USER
+// // }
 // package com.example.demo.entity;
 
 // public enum Role {
 //     ADMIN,
-//     USER
+//     PUBLISHER,
+//     SUBSCRIBER
 // }
-package com.example.demo.entity;
 
-public enum Role {
-    ADMIN,
-    PUBLISHER,
-    SUBSCRIBER
-}
