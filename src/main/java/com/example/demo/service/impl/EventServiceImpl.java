5ad510dcdcdf12af22ff748414ae94e2bddb5bd9
bala-amypo <bala@amypo.in>
2025-12-25 03:28:03 +0000

    User commit

--- src/main/java/com/example/demo/service/impl/EventServiceImpl.java
+++ src/main/java/com/example/demo/service/impl/EventServiceImpl.java
@@ -208,6 +208,7 @@ public class EventServiceImpl implements EventService {
             event.setIsActive(false);
             eventRepository.save(event);
         }
-
+      return event;
+      
     }
 }
