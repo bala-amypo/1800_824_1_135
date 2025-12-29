c9bde1b1683dbc921619f5bafd462727edff2d7b
bala-amypo <bala@amypo.in>
2025-12-29 08:36:38 +0000

    User commit

--- src/main/java/com/example/demo/controller/RecommendationController.java
+++ src/main/java/com/example/demo/controller/RecommendationController.java
@@ -49,5 +49,5 @@ public class RecommendationController {
 //@DeleteMapping("/{eventId}")
 // public void unsubscribe(@RequestParam Long userId,
 //                           @PathVariable Long eventId) {
-//         subscriptionService.unsubscribe(userId, eventId);
-//     }
+//        subscriptionService.unsubscribe(userId, eventId);
+// }
